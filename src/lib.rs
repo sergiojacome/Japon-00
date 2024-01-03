bfe4be8d5a80fff0a35cbb9a8f0f4a04c38ed878
Sergio Jacome <sergio.jacome@gmail.com>
2024-01-02 21:08:29 -0500

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -49,8 +49,6 @@ async fn handler(update: tg_flows::Update) {
             }
         };
 
-        let _ = tele.send_chat_action(chat_id, "pensando...".to_string()).await;
-
         let response = run_message(thread_id.as_str(), String::from(text)).await;
         _ = tele.send_message(chat_id, response);
     }
